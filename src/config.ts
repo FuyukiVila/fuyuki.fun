45ff4cf14663ebc5f923702dc7f7b0b9e34aef4b
FuyukiVila <1642421711@qq.com>
2024-12-12 22:15:08 +0800

    update SpeedInsights

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/FuyukiVila/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/fuyukivila/yuki.fuyuki.fun',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -65,7 +65,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/FuyukiVila',
+      url: 'https://github.com/fuyukivila',
     },
     {
       name: 'イルミネーション',
