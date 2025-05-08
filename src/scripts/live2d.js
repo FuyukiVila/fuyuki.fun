788950d09dec00bdf8d1ca2d1945e7b6e649346d
FuyukiVila <1642421711@qq.com>
2025-05-08 11:42:25 +0800

    fix: update asset URLs for waifu styles and icons

--- src/scripts/live2d.js
+++ src/scripts/live2d.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     try {
         const path = 'https://cdn.jsdelivr.net/gh/fuyukivila/live2d_demo@master/'
-        $("<link>").attr({ href: path + "@styles/waifu.css", rel: "stylesheet", type: "text/css" }).appendTo('head');
+        $("<link>").attr({ href: "@styles/waifu.css", rel: "stylesheet", type: "text/css" }).appendTo('head');
         $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
         $.ajax({
             url: path + "assets/waifu-tips.min.js", dataType: "script", cache: true, success: function () {
